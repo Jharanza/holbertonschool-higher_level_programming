1c784990566d7b319429343e41e229cb993ea518
jharanza <jharanza1986@gmail.com>
2023-07-15 08:45:31 -0700

    Adding condition

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,6 +2,6 @@
 import add_0 as add
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add.add(a, b)), end='')
+print("{} + {} = {}".format(a, b, add.add(a, b)))
 if __name__ == "__main__":
-    print()
+    import sys
