9cedc30871b4da882b52531ef1d1690f7a183c33
jharanza <jharanza1986@gmail.com>
2023-07-15 08:12:52 -0700

    Adding if__name-_

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import add_0 as ad
+import add_0 as add
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, ad.add(a, b)))
+print("{} + {} = {}".format(a, b, add.add(a, b)), end='')
+if __name__ == "__main__":
+    print()
