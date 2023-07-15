2696d73fb02f39dab949f1593a11d79b396116bc
jharanza <jharanza1986@gmail.com>
2023-07-15 08:51:16 -0700

    Add condition to no uñse

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,6 +2,7 @@
 import add_0 as add
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add.add(a, b)))
+print("{} + {} = {}".format(a, b, add.add(a, b)), end='')
 if __name__ == "__main__":
     import sys
+    print()
