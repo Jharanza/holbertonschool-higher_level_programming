fe6b24a777c17ca4d8a7be3d66b4f7940b9c5e89
jharanza <jharanza1986@gmail.com>
2023-07-16 22:10:02 -0700

    Checking the code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys as v
     arg = len(v.argv) - 1
     if arg == 1:
-        print(f"{arg} argument:") 
+        print(f"{arg} argument:")
     elif arg == 0:
         print(f"{arg} arguments.")
     else:
         print(f"{arg} arguments:")
-    for i in range(arg + 1):
+    for i in range(len(v.argv)):
         if i > 0:
             print(f"{i}: {v.argv[i]}")
