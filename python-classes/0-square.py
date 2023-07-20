6de34ebdce2a6bf991a583c32d193539703a92fd
jharanza <jharanza1986@gmail.com>
2023-07-20 13:05:48 -0700

    Defining a square

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    '''
+    Square: Class that define a square
+    __init__: Constructor of the class
+    self: Reference to the same method
+    '''
+    def __init__(self):
+        pass
