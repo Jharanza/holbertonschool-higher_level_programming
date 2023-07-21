8a320ef536fddf667fd2e33a64df709d05b43631
jharanza <jharanza1986@gmail.com>
2023-07-21 10:44:14 -0700

    Printing x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    m = ""
+    for i in range(x):
+        try:
+            m += str(my_list[i])
+            n += 1
+        except:
+            pass
+    print("{:s}".format(m))
+    return n
