a31cef9a2cec3d6fcd2bf89892574c89cae9b86f
jharanza <jharanza1986@gmail.com>
2023-07-23 09:27:18 -0700

    Defining a square with a size

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+    Moodule square
+'''
+
+
+class Square:
+    '''
+        clas square
+    '''
+    def __init__(self, size):
+        '''
+            Constructor class
+            Args:
+                size: the size of a square
+        '''
+        self.__size = size
