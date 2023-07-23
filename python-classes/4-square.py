3e322f8157c4c4f2e663ec453efac7df6ec81788
jharanza <jharanza1986@gmail.com>
2023-07-23 10:19:41 -0700

    Defining a square with methods setter and getter

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+    module 4-square
+'''
+
+
+class Square:
+    ''' class square '''
+
+    def __init__(self, size):
+        '''
+            Teh __init__ is the method Constructor.
+            Args:
+                size: The size of the square
+        '''
+        self.__size = size
+
+    @property
+    def size(self):
+        ''' The method getter to get the size of the square '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        ''' The method setter to set the size of the square '''
+        self.__size = value
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        if int(value) <= 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        ''' The method area return the area of the square '''
+        return int(self.__size) * int(self.__size)
