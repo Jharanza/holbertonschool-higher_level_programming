8f0574ac3c1f83ce4dbc07a69695658e45d698c2
jharanza <jharanza1986@gmail.com>
2023-07-23 09:48:40 -0700

    Validating the size of the square

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+    2-square module
+'''
+
+
+class Square:
+    ''' Square class '''
+
+    def __init__(self, size=0):    
+        ''' 
+            The __init__ method is a Constructor.
+            Arguments:
+                size (int): size of the square
+        '''
+        self.__size = size
+
+        if int(size) < 0:
+            raise ValueError("size must be >= 0")
+        if not type(size) is int:     
+            raise TypeError("size must be an integer")
