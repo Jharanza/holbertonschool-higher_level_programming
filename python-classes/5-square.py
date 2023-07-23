fdca5d86bb8da963501a89d7d67f3252c3e9c5c3
jharanza <jharanza1986@gmail.com>
2023-07-23 11:13:06 -0700

    Printing the square

---
+++ python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''
+    module 4-square
+'''
+
+
+class Square:
+    ''' class square '''
+
+    def __init__(self, size=0):
+        '''
+            Teh __init__ is the method Constructor.
+            Args:
+                size: The size of the square
+        '''
+        self.size = size
+
+    @property
+    def size(self):
+        ''' The method getter to get the size of the square '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        ''' The method setter to set the size of the square '''
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        if int(value) < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        ''' The method area return the area of the square '''
+        return int(self.__size) * int(self.__size)
+
+    def my_print(self):
+        ''' The method my_print print the square '''
+        if self.__size == 0:
+            print(" ")
+        else:
+            for x in range(self.__size):
+                symbol = ""
+                for j in range(self.__size):
+                    symbol += "#"
+                print(symbol)
