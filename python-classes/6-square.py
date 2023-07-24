9292becca339fde4a0f95315f9098a878e285d4e
jharanza <jharanza1986@gmail.com>
2023-07-23 18:53:52 -0700

    Fixing the conditional

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -38,10 +38,10 @@ class Square:
     @position.setter
     def position(self, value):
         ''' The setter method set he position with the valeu '''
-        if not isinstance(value, tuple) or len(self.__position) != 2:
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         x, y = value
-        if not isinstance(x, int) or not isinstance(y, int) or x < 0 or y < 0:
+        if not isinstance(x, int) or not isinstance(y, int) or x <= 0 or y <= 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
