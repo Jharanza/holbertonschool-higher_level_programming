4322a0d8e28419560c6d30e45406bdeb702a337b
jharanza <jharanza1986@gmail.com>
2023-07-27 10:40:07 -0700

    Deleting class

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,14 +3,12 @@
     module 0-lookup
 '''
 
-class My_class:
-    ''' My class returns methods and attributes of an object'''
-
-    def lookup(obj):
-        '''
-            lookup: This method returns the available methods and attributes
-            of an object.
-            Args:
-                obj: Only argumento to check
-        '''
-        return dir(obj)
+    
+def lookup(obj):
+    '''
+        lookup: This method returns the available methods and attributes
+        of an object.
+        Args:
+            obj: Only argumento to check
+    '''
+    return dir(obj)
