60245e1ff0bf095bba4c3d2f06322b609a9ab957
jharanza <jharanza1986@gmail.com>
2023-07-29 11:37:17 -0700

    Class with a method without implement

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+    module 6-base_geometry
+'''
+
+
+class BaseGeometry:
+    ''' class with a empty method '''
+
+    def area(self):
+        '''
+            method with a exception
+        '''
+        raise Exception("area() is not implemented")
