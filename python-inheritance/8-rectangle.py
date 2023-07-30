51bef323a44bdef17ea5fa9dbedc91b382bd1796
jharanza <jharanza1986@gmail.com>
2023-07-29 21:59:05 -0700

    class that inherits methods of a base class

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+    modulo 8-rectangle
+'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    ''' class Rectangle with constructor '''
+
+    def __init__(self, width, height):
+        '''
+            method constructor
+            Args:
+                width: width of the rectangle
+                height: height of the rectangle
+        '''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        super().__init__()
+        self.__width = width
+        self.__height = height
