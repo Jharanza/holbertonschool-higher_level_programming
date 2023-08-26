693baee21c006b25646b2c7ea21da0c974d737fa
jharanza <jharanza1986@gmail.com>
2023-08-26 13:08:31 -0700

    feat: Correcting column name

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- List all genres and the numbers of shows linked to each
 -- First column must be called genre and the second must be called number of shows
-SELECT a.name, COUNT(*) AS number_of_shows
-FROM tv_genres a 
+SELECT a.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres a  
 LEFT JOIN tv_show_genres b
 ON a.id = b.genre_id WHERE b.genre_id IS NOT NULL
 GROUP BY a.name ORDER BY number_of_shows DESC;
