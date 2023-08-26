656d7cc43ee8a79e2de8dd2377ae9c0343c792d7
jharanza <jharanza1986@gmail.com>
2023-08-26 13:05:06 -0700

    feat: Add condition to be not null

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@
 SELECT a.name, COUNT(*) AS number_of_shows
 FROM tv_genres a 
 LEFT JOIN tv_show_genres b
-ON a.id = b.genre_id
+ON a.id = b.genre_id WHERE b.genre_id IS NOT NULL
 GROUP BY a.name ORDER BY number_of_shows DESC;
