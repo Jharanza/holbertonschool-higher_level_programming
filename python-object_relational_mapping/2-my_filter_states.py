9fabaa94d47a1626a4778ab091717f5281d15439
jharanza <jharanza1986@gmail.com>
2023-08-27 16:53:35 -0700

    feat: Add binary condition

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     condition = "SELECT * FROM states\
-            WHERE name = '{}' ORDER BY id".format(sys.argv[4])
+            WHERE BINARY name = '{}' ORDER BY id".format(sys.argv[4])
 
     cursor.execute(condition)
 
