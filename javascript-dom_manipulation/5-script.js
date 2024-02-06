7f9b70c44a651c3b9209bfef339342382c6c398b
jhony arana <jharanza1986@gmail.com>
2024-02-06 15:16:37 -0500

    feat: change the inner text in one element

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,12 @@
+/**
+ * Update the text from the element header
+ * @header var that get the element header
+ * @update var that get the element with id update_header
+ */
+
+const header = document.querySelector('header');
+const update = document.querySelector('#update_header');
+
+update.addEventListener('click', () => {
+    header.innerText = 'New Header!!!';
+})
