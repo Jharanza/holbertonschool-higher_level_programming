b15042bd4071c6160dba3a1e0a587664580da18f
jhony arana <jharanza1986@gmail.com>
2024-02-13 12:22:31 -0500

    feat: get the value from an API with ajax

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,12 @@
+/**
+ * Fetch the character name from an API
+ * @$ call tot he function jQuery
+ * @req promise to the given url
+ */
+const $ = window.$;
+
+const req = $.ajax({
+  url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+  datatype: 'jsonp'
+});
+req.done(data => $('#character').text(data.name));
