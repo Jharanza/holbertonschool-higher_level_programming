74fd899c023048a5aedcc721dbf6e55557e30c2f
jhony arana <jharanza1986@gmail.com>
2024-03-17 17:16:20 -0500

    feat: Add a private attribute to the Square class

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Create a Square class with an attribute """
+
+class Square:
+    
+    def __init__(self, size) -> None:
+        """ Constructor """
+        self.__size = size
