257cac81e88275e4fdc269cb8b52adb23132b458
jhony arana <jharanza1986@gmail.com>
2024-03-17 18:19:15 -0500

    feat: Add a method that prints the square

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Create a Square class with a method to print the square"""
+
+class Square:
+    
+    def __init__(self, size=0) -> None:
+        """ Constructor """
+        self.__size = size
+        self.size = size
+
+    @property        
+    def size(self):
+        """ Getter """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter"""
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self) -> int:
+        """ Returns the square area """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ Print the square """
+        if self.size == 0:
+            print('')
+        else:
+            for _ in range(self.size):
+                print('#' * self.size)
