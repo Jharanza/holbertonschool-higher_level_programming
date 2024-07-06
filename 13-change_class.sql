2970243e7cc9c7f4433b409e1088422d3cdc3c05
jharanza <jharanza1986@gmail.com>
2024-07-06 11:54:51 -0500

    feat: re-do all tasks

---
+++ 13-change_class.sql
@@ -0,0 +1,3 @@
+-- Remove all records with a score <= 5 --
+
+DELETE second_table FROM second_table WHERE score <= 5;
---
+++ 16-no_link.sql
@@ -0,0 +1,3 @@
+-- lists all records that have not null value in name --
+
+SELECT score, name FROM second_table where name IS NOT NULL;
